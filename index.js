dad79d4d9c006d4174ee99db0a5e6274b2dd64cf
Elmacflai <arquitectoalfonso@gmail.com>
2024-08-06 21:56:23 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const button = document.querySelector("#getMovieList")
+
+const movieList = document.querySelector("#movieList")
+const displayMovies = ()=>{
+    const movieList = document.querySelector("#movieList")
+}
+
+
+
+const getHorrorMovies = () => {
+  const apiKey = "cb657900"
+  const searchQuery = "horror"
+  const apiEndPoint = `https://www.omdbapi.com/?s=${searchQuery}&apikey=${apiKey}`
+  //fetch es una funcion que nos permite hacer peticiones a una api, y devuelve algo que se llama promesa
+  fetch(apiEndPoint).then((response)=>{
+    return response.json()
+  }).then((data)=>{
+    const horrorMovies = data.search
+    console.log(data)
+  })
+}
+    
+button.addEventListener("click", getHorrorMovies)
